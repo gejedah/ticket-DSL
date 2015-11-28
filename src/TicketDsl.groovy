4f76a5fdf5854b51afaa47b04221b1d9059e30eb
kevin <kevin.zhong94@yahoo.com>
2015-11-29 01:07:01 +0700

    output src code raw

--- src/TicketDsl.groovy
+++ src/TicketDsl.groovy
@@ -3,24 +3,27 @@ import groovy.xml.MarkupBuilder
  * Processes a simple DSL to create various formats of a ticket: html
  */
 class TicketDsl {
-
     String vcompanyName
     String vmotto
     String vagentName
-    def optAttrs = []
-    def obligeAttrs = []
-    Vector<String> sections
 
     private static HashMap<String, Boolean> invocation_effect = new HashMap()
+    private static HashMap<String, List<Attr>> sections = new HashMap<>()
+    private static HashMap<String, List<String>> obligeAttrs = new HashMap<>()
 
     public TicketDsl(){
-        File file = new File("src/config.txt")
-        List<String> oblig_atts = file.readLines()
+        File file_attr = new File("src/config.txt")
+        List<String> oblig_atts = file_attr.readLines()
         for (String att : oblig_atts){
 //            println att
             invocation_effect.put(att, false)
         }
-        sections = new Vector<>()
+
+        File file_section = new File("src/config1.txt")
+        List<String> tempList = file_section.readLines()
+        for (String section: tempList){
+            sections.put(section, new ArrayList<Attr>())
+        }
     }
 
     /**
@@ -29,13 +32,27 @@ class TicketDsl {
      */
     def methodMissing(String methodName, args) {
 //        println("Method Missing being invoked")
-        def Attr = new Attr(name: methodName, vals: args)
         if (invocation_effect.containsKey(methodName)){
-            obligeAttrs << Attr
             invocation_effect.replace(methodName, true)
+            ArrayList tes = new ArrayList<>()
+            tes = args
+            tes.removeAll(Collections.singleton("")) // remove ""
+            obligeAttrs.put(methodName, tes)
+//            println "argumen pertama" + obligeAttrs.get(methodName).get(0)
         }
         else{
-            optAttrs << Attr
+            def Attr = new Attr(name: methodName, vals: args)
+            if (args.length >= 1){
+                if (sections.containsKey(args[0])){
+                    sections.get(args[0]).add(Attr)
+                }
+                else{
+                    sections.put(args[0], new ArrayList<Attr>().add(Attr))
+                }
+            }
+            else{
+                sections.get("etc").add(Attr)
+            }
         }
     }
 
@@ -95,9 +112,9 @@ class TicketDsl {
                     div(class: "container"){
                         div(class: "logo"){
                             img(src: "logo.png", alt: "Logo", align: "middle")
-                            h1(class: "brand", "Tiger Air")
-                            p(class: "tagline"){
-                                strong("We make people fly")
+                            h1(class: "brand required", id: "companyName", {obligeAttrs.get("companyName").isEmpty() ? "" : obligeAttrs.get("companyName").get(0)}.call())
+                            p(class: "tagline required", id: "motto"){
+                                strong({obligeAttrs.get("motto").isEmpty() ? "" : obligeAttrs.get("motto").get(0)}.call())
                             }
                         }
                         div(class: "clear")
@@ -106,14 +123,14 @@ class TicketDsl {
                             table(width: "100%"){
                                 tr(){
                                     td("Agent Name")
-                                    td("PT. Trinusa Travelindo")
+                                    td(class: "required", id: "agentName", {obligeAttrs.get("agentName").isEmpty() ? "" : obligeAttrs.get("agentName").get(0)}.call())
                                     td("Issued Date")
-                                    td("Tanggal")
+                                    td(class: "required", id: "issuedDate", {obligeAttrs.get("issuedDate").isEmpty() ? "" : obligeAttrs.get("issuedDate").get(0)}.call())
                                 }
                                 tr(){
                                     td("Booking Reference")
                                     td{
-                                        strong("NLAWB")
+                                        strong(class: "required", id: "bookRef", {obligeAttrs.get("bookRef").isEmpty() ? "" : obligeAttrs.get("bookRef").get(0)}.call())
                                     }
                                 }
                             }
@@ -125,14 +142,17 @@ class TicketDsl {
                                     th("Name")
                                     th("eTicket Number")
                                 }
-                                tr(){
-                                    td("pet")
-                                    td("72109212924124")
+                                Closure tes = {
+                                    int i = 0
+                                    while (i < obligeAttrs.get("passengerName").size()){
+                                        tr(){
+                                            td(class: "required", id: "passengerName $i", {obligeAttrs.get("passengerName").isEmpty() ? "" : obligeAttrs.get("passengerName").get(i)}.call())
+                                            td(class: "required", id: "eTicketNum $i", {obligeAttrs.get("eTicketNum").isEmpty() ? "" : obligeAttrs.get("eTicketNum").get(i)}.call())
+                                        }
+                                        i++
+                                    }
                                 }
-                                tr(){
-                                    td("Mrs. Jeanice Ginting")
-                                    td("97009249741123")
-                                }
+                                tes.call()
                             }
                         }
                         div(class: "itinerary-details"){
@@ -148,16 +168,23 @@ class TicketDsl {
                                     th("Class")
                                     th("Bagg.")
                                 }
-                                tr(){
-                                    td("21 APR 2016")
-                                    td()
-                                    td()
-                                    td()
-                                    td()
-                                    td()
-                                    td()
-                                    td()
+                                Closure tes = {
+                                    int i = 0
+                                    while (i<obligeAttrs.get("iteneraryDate").size()){
+                                        tr(){
+                                            td(class: "required", id: "iteneraryDate $i", {obligeAttrs.get("iteneraryDate").isEmpty() ? "" : obligeAttrs.get("iteneraryDate").get(i)}.call())
+                                            td(class: "required", id: "iteneraryFlight $i", {obligeAttrs.get("iteneraryFlight").isEmpty() ? "" : obligeAttrs.get("iteneraryFlight").get(i)}.call())
+                                            td(class: "required", id: "iteneraryDepartAirport $i", {obligeAttrs.get("iteneraryDepartAirport").isEmpty() ? "" : obligeAttrs.get("iteneraryDepartAirport").get(i)}.call())
+                                            td(class: "required", id: "iteneraryArriveAirport $i", {obligeAttrs.get("iteneraryArriveAirport").isEmpty() ? "" : obligeAttrs.get("iteneraryArriveAirport").get(i)}.call())
+                                            td(class: "required", id: "iteneraryDepartTime $i", {obligeAttrs.get("iteneraryDepartTime").isEmpty() ? "" : obligeAttrs.get("iteneraryDepartTime").get(i)}.call())
+                                            td(class: "required", id: "iteneraryArrivetime $i", {obligeAttrs.get("iteneraryArrivetime").isEmpty() ? "" : obligeAttrs.get("iteneraryArrivetime").get(i)}.call())
+                                            td(class: "required", id: "iteneraryClass $i", {obligeAttrs.get("iteneraryClass").isEmpty() ? "" : obligeAttrs.get("iteneraryClass").get(i)}.call())
+                                            td(class: "required", id: "iteneraryBaggage $i", {obligeAttrs.get("iteneraryBaggage").isEmpty() ? "" : obligeAttrs.get("iteneraryBaggage").get(i)}.call())
+                                        }
+                                        i++
+                                    }
                                 }
+                                tes.call()
                             }
                         }
                         div(class: "payment-details"){
@@ -165,21 +192,21 @@ class TicketDsl {
                             table(width: "100%"){
                                 tr(){
                                     td(class: "payment-table-left-column", "Nett Fare")
-                                    td(class: "payment-table-right-column", "IDR 2500000")
+                                    td(class: "payment-table-right-column required", id: "paymentNett", {obligeAttrs.get("paymentNett").isEmpty() ? "" : obligeAttrs.get("paymentNett").get(0)}.call())
                                 }
                                 tr(){
                                     td(class: "payment-table-left-column", "Taxes")
-                                    td(class: "payment-table-right-column", "IDR 250000")
+                                    td(class: "payment-table-right-column required", id: "paymentTaxes", {obligeAttrs.get("paymentTaxes").isEmpty() ? "" : obligeAttrs.get("paymentTaxes").get(0)}.call())
                                 }
                                 tr(){
                                     td(class: "payment-table-left-column", "Total")
-                                    td(class: "payment-table-right-column", "IDR 2750000")
+                                    td(class: "payment-table-right-column required", id: "total")
                                 }
                             }
                         }
                         div(class: "notice"){
                             h3(class: "notice-label", "Notice")
-                            p("Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper. Cras justo odio, dapibus ac facilisis in, egestas eget quam.")
+                            p(class: "required", id: "notice", {obligeAttrs.get("notice").isEmpty() ? "" : obligeAttrs.get("notice").get(0)}.call())
                         }
                     }
                 }
@@ -201,13 +228,13 @@ class TicketDsl {
         String[] param
 
         while ((input = bfReader.readLine()) != null && input.length()!= 0 && input != "exit") {
-            inputs = input.split("\\s+")
+            inputs = input.split("[<>]")
             if (input.length() > 1){
                 param = Arrays.copyOfRange(inputs, 1, inputs.length)
-//            println "byk elemen yg diinput: " + inputs.length
-//            for (int i = 0; i < inputs.length; i++) {
-//                println "Elemen ke $i adalah " + inputs[i]
-//            }
+//                println "byk elemen yg diinput: " + inputs.length
+//                for (int i = 0; i < inputs.length; i++) {
+//                    println "Elemen ke $i adalah " + inputs[i]
+//                }
             }
             ticketDsl.invokeMethod(inputs[0], param)
 //            println "Masukkan input anda: "
